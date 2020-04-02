ef6972c62c799241070f19e9c0b17910a5c25584
Orane Ellis <57927175+ellisorane@users.noreply.github.com>
2020-04-01 21:31:06 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -24,20 +24,6 @@ const scrollReveal = () => {
     duration: 2000,
   })
 
-  //Projects
-  sr.reveal('.slide-right', {
-    duration: 2000,
-    origin: 'right',
-    distance: '300px',
-    viewFactor: 0.2 //When we see 2% of this div the animation will start
-  })
-
-  //Contacts
-  sr.reveal('.cnt-fade-in', {
-    duration: 2000,
-    viewFactor: 0.2 
-
-  })
 }; 
 
 scrollReveal();
